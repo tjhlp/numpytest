12272a407d7e72730116e33765f4d2b33f69a78d
tjhlp <374586186@qq.com>
2019-07-24 16:03:42 +0800

    pandas_test2

--- numpytest/pandas/pandas_tjh_02.py
+++ numpytest/pandas/pandas_tjh_02.py
@@ -1,11 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import datetime
+import pandas_datareader.data as web
+import fix_yahoo_finance as yf
+yf.pdr_override()
 
-# fig = plt.figure()
-fig = plt.figure(figsize=(3, 6))  # 指定画图区大小（长，宽）
-ax1 = fig.add_subplot(2, 1, 1)
-ax2 = fig.add_subplot(2, 1, 2)
-
-ax1.plot(np.random.randint(1, 5, 5), np.arange(5))  # 第一个子图画图
-ax2.plot(np.arange(10) * 3, np.arange(10))  # 第二个子图画图
-plt.show()
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2015, 8, 22)
+df = web.get_data_yahoo('AAPL', start, end)
+print(df)
