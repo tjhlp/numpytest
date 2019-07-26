a67ed19870827d5de449874ac9d177cf338a5fc3
tjhlp <374586186@qq.com>
2019-07-26 19:34:57 +0800

    matplotlib_test3

--- numpytest/matplotlib/matplotlib_tjh_02.py
+++ numpytest/matplotlib/matplotlib_tjh_02.py
@@ -6,10 +6,20 @@ x = np.linspace(-1, 1, 50)
 print(x)
 y1 = 2 * x + 1
 y2 = x ** 2
-plt.figure()
+plt.figure(num=1)
+# image
 plt.plot(x, y1, label='linear')
-plt.plot(x, y2, label='cubic')
-
-plt.xlabel('x label')
-plt.xlabel('y label')
+plt.plot(x, y2, color='red', label='cubic',linestyle='--')
+# limit
+plt.xlim(-1, 2)
+plt.ylim(-2, 3)
+ticks = np.linspace(-1,2,5)
+plt.xticks(ticks)
+plt.yticks([-2, -1, 1, 2],
+           [r'$really\ bad$', '$bad$', '$well$', '$really\ well$']
+           )
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+plt.legend(loc='best')
 plt.show()
---
+++ numpytest/matplotlib/matplotlib_tjh_03.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure()
+n = 1024
+X = np.random.normal(0, 1, n)
+Y = np.random.normal(0, 1, n)
+T = np.arctan2(X, Y)
+
+plt.scatter(X, Y, s=75, c=T, alpha=0.5)
+plt.xlim(-1.5, 1.5)
+plt.xticks(())
+plt.ylim(-1.5, 1.5)
+plt.xticks(())
+plt.show()
