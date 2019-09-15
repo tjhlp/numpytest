1d83a6dc69cdc49d69b61cbbde298b0c1b108948
tjhlp <374586186@qq.com>
2019-09-15 09:33:59 +0800

    jupyter 学习knn

---
+++ numpytest/knn.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = load_iris()
+# print("鸢尾花数据集的返回值:\n", iris)
+# print(iris.data)
+# print(iris.target)
+print(iris.feature_names)
+print(iris.DESCR)
+
+
