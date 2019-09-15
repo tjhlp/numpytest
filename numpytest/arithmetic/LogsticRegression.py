f5fb19f71ba9b854d40af45b2cbf4e89971edc04
tjhlp <374586186@qq.com>
2019-09-15 20:30:07 +0800

    逻辑回归

---
+++ numpytest/arithmetic/LogsticRegression.py
@@ -0,0 +1,17 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+
+# 获取数据
+names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape',
+                   'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin',
+                   'Normal Nucleoli', 'Mitoses', 'Class']
+data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data",
+                   names=names)
+
+print(data.head())
+
+
+
