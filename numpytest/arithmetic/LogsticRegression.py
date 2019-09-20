36858a7a095db3a8bf85a940ea12f1875443a39c
tjhlp <374586186@qq.com>
2019-09-20 19:09:18 +0800

    逻辑回归

--- numpytest/arithmetic/LogsticRegression.py
+++ numpytest/arithmetic/LogsticRegression.py
@@ -7,7 +7,7 @@ import numpy as np
 # 获取数据
 names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape',
                    'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin',
-                   'Normal Nucleoli', 'Mitoses', 'Class']
+                 'Normal Nucleoli', 'Mitoses', 'Class']
 data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data",
                    names=names)
 
